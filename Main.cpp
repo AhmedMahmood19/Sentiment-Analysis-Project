034e31a7870c976e4ca62dd9308dfa784df567b5
MuhammadQasimFuzail <qasimfuzail@gmail.com>
2021-12-11 21:15:47 +0500

    json to map with title as key and a list of reviews as value

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,10 @@
 #include <iomanip>
 #include "nlohmann/json.hpp"
 #include <fstream>
+#include <map>
+#include <list>
+#include <iterator>
+
 
 
 using json = nlohmann::json;
@@ -10,12 +14,47 @@ using namespace std;
 int main()
 {
     // create a JSON object
-    ifstream i("Amazon-Datasets\\kindle_store_reviews\\test2.json");
+    ifstream reviewFile("Files\\Used Files\\kindle_store_reviews.json");
     json t;
-    i >> t;
+    map<string,list<string>>  M;
 
+    reviewFile >> t;
+   
+    
+    for (int i=0;i<t.size()-1;)
+    {
+        string temp= t[i]["title"];
+        list<string> l;
+        
+
+        while (t[i]["title"]==temp)
+        {
+            if(!t[i]["reviewText"].is_null())
+                l.push_back(t[i]["reviewText"]);
+
+            i++;
+            if (i == 2362)
+                cout << "kuch" << endl;
+        }
+        //M.insert(pair<string, list<string>>(temp, l));
+        M[temp] = l;
+        
+    }
+    
+  
+
+    for (pair<const string,list<string>>& x: M)
+    {
+        cout << x.first<<endl;
+        for (auto v: x.second)
+        {
+            cout << v<< endl;
+        }
+        cout << endl << " --------------------------------------------------------------------------- " << endl;
+
+    }
+    
  
     
-    cout << setw(4) << t[2238]["pages"] << '\n';
 
 }
